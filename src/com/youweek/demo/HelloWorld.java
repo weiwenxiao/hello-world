46e58db8d50cf3233e06eccfaf886beaf2c4c6c4
yangy <199662618@qq.com>
2016-05-03 15:04:35 +0800

    你好牛逼啊！

--- src/com/youweek/demo/HelloWorld.java
+++ src/com/youweek/demo/HelloWorld.java
@@ -6,6 +6,7 @@ package com.youweek.demo;
 public class HelloWorld {
 
     public static void main(String[] args){
-        System.out.print("hello-world");
+        System.out.println("hello-world");
+        System.out.print("hello git!");
     }
 }
