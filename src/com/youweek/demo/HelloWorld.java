3fc3bb3e5b277f8f683aa34be4063cd3e4bb9748
yangy <199662618@qq.com>
2016-05-03 15:11:01 +0800

    good job!

--- src/com/youweek/demo/HelloWorld.java
+++ src/com/youweek/demo/HelloWorld.java
@@ -8,5 +8,7 @@ public class HelloWorld {
     public static void main(String[] args){
         System.out.println("hello-world");
         System.out.print("hello git!");
+        System.out.print("hello git111111!");
+
     }
 }
