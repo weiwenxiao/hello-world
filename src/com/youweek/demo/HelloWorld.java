868e4950f042d5abf90e8c76af08c66816100801
yangy <199662618@qq.com>
2016-05-03 14:54:49 +0800

    Initial commit

---
+++ src/com/youweek/demo/HelloWorld.java
@@ -0,0 +1,11 @@
+package com.youweek.demo;
+
+/**
+ * Created by Administrator on 2016/5/3 0003.
+ */
+public class HelloWorld {
+
+    public static void main(String[] args){
+        System.out.print("hello-world");
+    }
+}
